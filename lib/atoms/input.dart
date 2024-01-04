1bab4ac3ee2990ad9091053a57fec97720f92b53
Walee Sebiiy <waleesebiiy@gmail.com>
2024-01-04 01:14:24 +0100

    create login form

--- lib/atoms/input.dart
+++ lib/atoms/input.dart
@@ -1,32 +1,71 @@
 import 'package:flutter/material.dart';
 
 class Input extends StatelessWidget {
+  final String? label;
   final String placeholder;
+  final IconData? icon;
+  final TextInputType? keyboardType;
+  final bool isPassword;
+
+  final String? Function(String?)? validator;
+  final void Function(String?)? onSaved;
 
   const Input({
-    super.key,
+    Key? key,
+    this.label,
+    this.icon,
     required this.placeholder,
-  });
+    this.keyboardType = TextInputType.text,
+    this.isPassword = false,
+    this.validator,
+    this.onSaved,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      keyboardType: TextInputType.text,
-      decoration: InputDecoration(
-        hintText: placeholder,
-        filled: true,
-        fillColor: Theme.of(context).colorScheme.surfaceVariant,
-        focusedBorder: const OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.transparent),
-          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (label != null)
+          Text(
+            label!,
+            style: Theme.of(context).textTheme.bodyLarge,
+          ),
+        const SizedBox(height: 8.0),
+        TextFormField(
+          decoration: InputDecoration(
+            fillColor: Theme.of(context).colorScheme.surfaceVariant,
+            focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.transparent),
+              borderRadius: BorderRadius.all(Radius.elliptical(8.0, 8.0)),
+            ),
+            enabledBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.transparent),
+              borderRadius: BorderRadius.all(Radius.elliptical(8.0, 8.0)),
+            ),
+            errorBorder: const OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.elliptical(8.0, 8.0)),
+              borderSide: BorderSide(
+                color: Colors.transparent,
+              ),
+            ),
+            focusedErrorBorder: const OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.elliptical(8.0, 8.0)),
+              borderSide: BorderSide(
+                color: Colors.transparent,
+              ),
+            ),
+            filled: true,
+            isDense: true,
+            hintText: placeholder,
+            prefixIcon: icon != null ? Icon(icon) : null,
+          ),
+          validator: validator,
+          onSaved: onSaved,
+          obscureText: isPassword,
+          keyboardType: keyboardType,
         ),
-        enabledBorder: const OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.transparent),
-          borderRadius: BorderRadius.all(Radius.circular(10.0)),
-        ),
-      ),
-      // validator: validator,
-      // onSaved: onsaved,
+      ],
     );
   }
 }
--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:frema/atoms/input.dart';
-import 'package:frema/composable/auth-app-bar.dart';
-import 'package:frema/screen/dashboard.dart';
+import 'package:frema/models/user-login.dart';
+import 'package:frema/screen/projects.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -11,24 +11,66 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final key = GlobalKey<FormState>();
+  var userLogin = UserLogin();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: const AuthAppBar(),
         body: Container(
-          padding: const EdgeInsets.only(top: 42.0, left: 10.0, right: 10.0),
-          child: Column(
-            children: [
-              const Input(),
-              const Input(),
-              FilledButton(
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => const Dashboard()));
+            padding: const EdgeInsets.only(top: 42.0, left: 24.0, right: 24.0),
+            child: Form(
+              key: key,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              child: Column(children: [
+                Input(
+                  placeholder: 'Enter your email',
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Email is required';
+                    }
+                    return null;
                   },
-                  child: const Text("login"))
-            ],
-          ),
-        ));
+                  onSaved: (String? value) => userLogin.setEmail = value!,
+                ),
+                const Padding(padding: EdgeInsets.only(top: 18.0)),
+                Input(
+                  placeholder: 'Enter your password',
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Password is required';
+                    }
+                    return null;
+                  },
+                  isPassword: true,
+                  onSaved: (String? value) => userLogin.setPassword = value!,
+                ),
+                const Padding(padding: EdgeInsets.only(top: 48.0)),
+                SizedBox(
+                  width: double.infinity,
+                  height: 48.0,
+                  child: FilledButton(
+                      onPressed: () {
+                        if (key.currentState!.validate()) {
+                          key.currentState!.save();
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => const Project(),
+                            ),
+                          );
+                        } else {
+                          print("error");
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                      child: const Text("Sign in",
+                          style: TextStyle(fontSize: 16))),
+                ),
+              ]),
+            )));
   }
 }
